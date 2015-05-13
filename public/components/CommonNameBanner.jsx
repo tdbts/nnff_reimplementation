4591d6e202a0f174497e74ae1565bdec43372e28
tdbts <VinnySanchez87@gmail.com>
2015-05-13 15:42:17 -0400

    Add container around CommonNameBanner and make its height responsive.

--- public/components/CommonNameBanner.jsx
+++ public/components/CommonNameBanner.jsx
@@ -2,11 +2,48 @@ var React = require('react'),
 	$ = window.jQuery || require('jquery');
 
 var CommonNameBanner = React.createClass({
+	getInitialState: function () {
+		return {
+			bannerContainerHeight: {
+				height: 0
+			}
+		};
+	}, 
+
+	getDefaultProps: function () {
+		return {
+			banner: $('#common_name_banner')
+		};
+	}, 
+
+	setBannerContainerHeight: function () {
+		var banner = $('#common_name_banner');
+
+		this.setState({
+			bannerContainerHeight: {
+				height: banner.height() + parseInt(banner.css('padding-top')) + parseInt(banner.css('padding-bottom'))
+			}
+		});
+	}, 
+
+	componentDidMount: function () {
+		
+		this.setBannerContainerHeight();
+
+		window.addEventListener('resize', this.setBannerContainerHeight);
+	}, 
+
+	componentWillUnmount: function () {
+		window.removeEventListener('resize', this.setBannerContainerHeight);
+	},
+
 	render: function () {
 		return (
-			<div id="common_name_banner">
-				<h1 id="banner_foundation_name"><span id="national_text" className="text_block">The National </span><span id="banner_latin_name" className="text_block">Necrotizing Fasciitis</span><span id="foundation_text" className="text_block"> Foundation</span></h1>
-				<h4 id="banner_common_name">Commonly known as the &ldquo;Flesh-Eating&rdquo; Bacteria</h4>
+			<div id="common_name_banner_container" style={this.state.bannerContainerHeight}>
+				<div id="common_name_banner">
+					<h1 id="banner_foundation_name"><span id="national_text" className="text_block">The National </span><span id="banner_latin_name" className="text_block">Necrotizing Fasciitis</span><span id="foundation_text" className="text_block"> Foundation</span></h1>
+					<h4 id="banner_common_name">Commonly known as the &ldquo;Flesh-Eating&rdquo; Bacteria</h4>
+				</div>
 			</div>
 		);
 	}
--- public/javascripts/CommonNameBanner.js
+++ public/javascripts/CommonNameBanner.js
@@ -2,11 +2,48 @@ var React = require('react'),
 	$ = window.jQuery || require('jquery');
 
 var CommonNameBanner = React.createClass({displayName: "CommonNameBanner",
+	getInitialState: function () {
+		return {
+			bannerContainerHeight: {
+				height: 0
+			}
+		};
+	}, 
+
+	getDefaultProps: function () {
+		return {
+			banner: $('#common_name_banner')
+		};
+	}, 
+
+	setBannerContainerHeight: function () {
+		var banner = $('#common_name_banner');
+
+		this.setState({
+			bannerContainerHeight: {
+				height: banner.height() + parseInt(banner.css('padding-top')) + parseInt(banner.css('padding-bottom'))
+			}
+		});
+	}, 
+
+	componentDidMount: function () {
+		
+		this.setBannerContainerHeight();
+
+		window.addEventListener('resize', this.setBannerContainerHeight);
+	}, 
+
+	componentWillUnmount: function () {
+		window.removeEventListener('resize', this.setBannerContainerHeight);
+	},
+
 	render: function () {
 		return (
-			React.createElement("div", {id: "common_name_banner"}, 
-				React.createElement("h1", {id: "banner_foundation_name"}, React.createElement("span", {id: "national_text", className: "text_block"}, "The National "), React.createElement("span", {id: "banner_latin_name", className: "text_block"}, "Necrotizing Fasciitis"), React.createElement("span", {id: "foundation_text", className: "text_block"}, " Foundation")), 
-				React.createElement("h4", {id: "banner_common_name"}, "Commonly known as the “Flesh-Eating” Bacteria")
+			React.createElement("div", {id: "common_name_banner_container", style: this.state.bannerContainerHeight}, 
+				React.createElement("div", {id: "common_name_banner"}, 
+					React.createElement("h1", {id: "banner_foundation_name"}, React.createElement("span", {id: "national_text", className: "text_block"}, "The National "), React.createElement("span", {id: "banner_latin_name", className: "text_block"}, "Necrotizing Fasciitis"), React.createElement("span", {id: "foundation_text", className: "text_block"}, " Foundation")), 
+					React.createElement("h4", {id: "banner_common_name"}, "Commonly known as the “Flesh-Eating” Bacteria")
+				)
 			)
 		);
 	}
