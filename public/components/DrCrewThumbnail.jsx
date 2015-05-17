ae629518e84ed9a27638c4908da81079c4657b36
tdbts <VinnySanchez87@gmail.com>
2015-05-16 23:37:06 -0400

    More tweaks on responsiveness of panel inset.

--- public/components/DrCrewThumbnail.jsx
+++ public/components/DrCrewThumbnail.jsx
@@ -9,7 +9,7 @@ var DrCrewThumbnail = React.createClass({
 						<img id="dr_crew_image" src="images/dr_john_crew_unaltered.jpg" />
 					</div>
 					<div className="caption">
-						<h4>John Crew, M.D. <br />Seton Medical Center <br />Daly City, California </h4>
+						<h4 className="keep_same_line"><strong>John Crew, M.D. </strong></h4><h5><span className="keep_same_line">Seton Medical Center </span><span className="keep_same_line">Daly City, California </span></h5>
 					</div>
 					<div id="dr_crew_thumbnail_text_container">
 						<p id="dr_crew_thumbnail_text">Dr. Crew was nominated earlier this year for the Lister Legacy Prize for his groundbreaking, life and limb-saving treatment of NF. This award presented by the Royal College of Surgeons of Edinburgh to recognize contributions in controlling infections.</p>
@@ -18,7 +18,7 @@ var DrCrewThumbnail = React.createClass({
 				<div id="dr_crew_alt_thumbnail_container" className="thumbnail visible-sm-inline-block">	
 					<img id="dr_crew_alt_image" src="images/dr_john_crew_unaltered.jpg" />
 					<div className="caption">
-						<h4>John Crew, M.D. <br />Seton Medical Center <br />Daly City, California </h4>					
+						<h4 className="keep_same_line"><strong>John Crew, M.D. </strong></h4><h5><span className="keep_same_line">Seton Medical Center </span><span className="keep_same_line">Daly City, California </span></h5>
 						<p id="dr_crew_thumbnail_text">Dr. Crew was nominated earlier this year for the Lister Legacy Prize for his groundbreaking, life and limb-saving treatment of NF. This award presented by the Royal College of Surgeons of Edinburgh to recognize contributions in controlling infections.</p>
 					</div>
 				</div>				
--- public/javascripts/DrCrewThumbnail.js
+++ public/javascripts/DrCrewThumbnail.js
@@ -9,7 +9,7 @@ var DrCrewThumbnail = React.createClass({displayName: "DrCrewThumbnail",
 						React.createElement("img", {id: "dr_crew_image", src: "images/dr_john_crew_unaltered.jpg"})
 					), 
 					React.createElement("div", {className: "caption"}, 
-						React.createElement("h4", null, "John Crew, M.D. ", React.createElement("br", null), "Seton Medical Center ", React.createElement("br", null), "Daly City, California ")
+						React.createElement("h4", {className: "keep_same_line"}, React.createElement("strong", null, "John Crew, M.D. ")), React.createElement("h5", null, React.createElement("span", {className: "keep_same_line"}, "Seton Medical Center "), React.createElement("span", {className: "keep_same_line"}, "Daly City, California "))
 					), 
 					React.createElement("div", {id: "dr_crew_thumbnail_text_container"}, 
 						React.createElement("p", {id: "dr_crew_thumbnail_text"}, "Dr. Crew was nominated earlier this year for the Lister Legacy Prize for his groundbreaking, life and limb-saving treatment of NF. This award presented by the Royal College of Surgeons of Edinburgh to recognize contributions in controlling infections.")
@@ -18,7 +18,7 @@ var DrCrewThumbnail = React.createClass({displayName: "DrCrewThumbnail",
 				React.createElement("div", {id: "dr_crew_alt_thumbnail_container", className: "thumbnail visible-sm-inline-block"}, 	
 					React.createElement("img", {id: "dr_crew_alt_image", src: "images/dr_john_crew_unaltered.jpg"}), 
 					React.createElement("div", {className: "caption"}, 
-						React.createElement("h4", null, "John Crew, M.D. ", React.createElement("br", null), "Seton Medical Center ", React.createElement("br", null), "Daly City, California "), 					
+						React.createElement("h4", {className: "keep_same_line"}, React.createElement("strong", null, "John Crew, M.D. ")), React.createElement("h5", null, React.createElement("span", {className: "keep_same_line"}, "Seton Medical Center "), React.createElement("span", {className: "keep_same_line"}, "Daly City, California ")), 
 						React.createElement("p", {id: "dr_crew_thumbnail_text"}, "Dr. Crew was nominated earlier this year for the Lister Legacy Prize for his groundbreaking, life and limb-saving treatment of NF. This award presented by the Royal College of Surgeons of Edinburgh to recognize contributions in controlling infections.")
 					)
 				)				
